7a7835ac139fafb50b1575c19fdca9f97f1fa30a
Benjamin <benjamin.nutter@gmail.com>
2018-02-13 15:04:16 -0500

    remove ArgumentCheck

--- R/jagsBayesianModel.R
+++ R/jagsBayesianModel.R
@@ -46,17 +46,13 @@ jagsBayesianModel <- function (model = NULL, ... ) {
   #   if(!grepl("dbeta", argument[[1]])) flag_dbeta <- TRUE
   # }
   #
-  # Check <- checkmate::makeAssertCollection()
-  #
   # if (flag_dnorm | flag_dunif | flag_dbeta)
-  #   Check$push(
+  #   warning(
   #     "It seems that you are not using dnorm(mean, sd),  dunif(min, max)
   # or dbeta(a, b) as priors, or they are not correctly written. Please check
   # the arguments")
   #   # message(strwrap("It seems that you are not using dnorm(mean, sd), \n
   #   # dunif(min, max) or dbeta(a, b) as prior, are you sure?"))
-  #
-  # checkmate::reportAssertions(Check)
 
   if (is.null(model))
     return(jagsTwoBaselinesFull(...))
